7318b877dd221c82d76f8847563b0a08d6a87877
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-02 12:35:02 +0530

    Removing attendance menu when user is not logged in.

--- app/helpers/Menu.php
+++ app/helpers/Menu.php
@@ -96,6 +96,7 @@ class Menu extends Phalcon\Mvc\User\Component
 		else {
 			unset($this->_headerMenu['pull-left']['project']);
 			unset($this->_headerMenu['pull-left']['task']);
+			unset($this->_headerMenu['pull-left']['attendance']);
 			unset($this->_headerMenu['pull-right']['my_account']);
 			unset($this->_headerMenu['pull-right']['logout']);
 		}
