702d0297a491c1e0b069b7c0b665b608f27e1a0b
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-02 17:22:45 +0530

    Displaying remaining hours in the work report.

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -2,6 +2,54 @@
 
 class ReportController extends ControllerBase
 {
+	protected function _getRemainingHours($task_id, $date, $assigned_hours)
+	{
+		if ($assigned_hours == '00:00:00') {
+			return '00:00:00';
+		}
+
+		$time_1 = $assigned_hours;
+		$date_1 = explode(':', $time_1);
+		$timestamp_1 = ($date_1[0]*60*60)+($date_1[1]*60)+$date_1[2];
+
+		$results = $this->modelsManager->executeQuery('SELECT SEC_TO_TIME( SUM( TIME_TO_SEC( Attendance.total ) ) ) AS task_time FROM Attendance WHERE task_id = "' . $task_id . '" AND date <= "' . $date . '"');
+		$time_2 = '00:00:00';
+
+		foreach ($results AS $result) {
+			if (!is_null($result->task_time)) {
+				$time_2 = $result->task_time;
+			}
+		}
+
+		$date_2 = explode(':', $time_2);
+		$timestamp_2 = ($date_2[0]*60*60)+($date_2[1]*60)+$date_2[2];
+
+		if ($timestamp_1 > $timestamp_2) {
+			$diff = $timestamp_1 - $timestamp_2;
+			$minus = false;
+		}
+		else {
+			$diff = $timestamp_2 - $timestamp_1;
+			$minus = true;
+		}
+
+		$days = (int)gmdate("j", $diff) - 1;
+		$time_3 = gmdate("H:i:s", $diff);
+		$date_3 = explode(':', $time_3);
+
+		$hours = ($days * 24) + (int)$date_3[0];
+		$minutes = (int)$date_3[1];
+		$seconds = (int)$date_3[2];
+
+		$return = $hours . ':' . $minutes . ':' . $seconds;
+
+		if ($minus) {
+			$return = '-' . $return;
+		}
+
+		return $return;
+	}
+
 	protected function _getWorkReport($user_id, $from, $to)
 	{
 		$reports = array();
@@ -35,6 +83,7 @@ class ReportController extends ControllerBase
 							$temp['task_title'] = $task->title;
 							$temp['time_spent'] = $result->task_time;
 							$temp['assigned_time'] = $task->hours;
+							$temp['time_remaining'] = $this->_getRemainingHours($task_id, $date, $task->hours);
 
 							if ($date == $task->completed_on) {
 								$temp['completed'] = 'Yes';
