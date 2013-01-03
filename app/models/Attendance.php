e978f771b27103c9f12b9da29ff885208d6216bf
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-04 00:57:14 +0530

    Displaying the current task logged in as a tool tip over the end time button.

--- app/models/Attendance.php
+++ app/models/Attendance.php
@@ -12,5 +12,18 @@ class Attendance extends Phalcon\Mvc\Model
     public function initialize()
     {
         $this->belongsTo('user_id', 'User', 'id');
+        $this->belongsTo('task_id', 'Task', 'id');
+    }
+
+    public function getTaskTitleText()
+    {
+        if ($this->task_id == 0) {
+            return "No Task";
+        }
+
+        $task = $this->getTask();
+        $project = $task->getProject();
+
+        return $project->name . ' - ' . $task->title;
     }
 }
