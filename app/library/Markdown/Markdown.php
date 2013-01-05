9378ea38cddebfcda5602ffd7726698b8bca16ab
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-05 16:12:59 +0530

    Making markdown generated code HTML compliant instead of XHTML

--- app/library/Markdown/Markdown.php
+++ app/library/Markdown/Markdown.php
@@ -20,7 +20,7 @@ define( 'MARKDOWN_VERSION',  "1.0.1o" ); # Sun 8 Jan 2012
 #
 
 # Change to ">" for HTML output
-@define( 'MARKDOWN_EMPTY_ELEMENT_SUFFIX',  " />");
+@define( 'MARKDOWN_EMPTY_ELEMENT_SUFFIX',  ">");
 
 # Define the width of a tab for code blocks.
 @define( 'MARKDOWN_TAB_WIDTH',     4 );
