333f03594a7b6e24105c23e9cd202beccc2b38ee
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-11 00:31:26 +0530

    Displaying the data for dashboard sub nav menu.

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -4,6 +4,28 @@ class DashboardController extends ControllerBase
 {
     public function indexAction()
     {
+    	$openTasksCount = $this->currentUser->getOpenTasksCount();
+    	$allTasksCount = $this->currentUser->getAllTasksCount();
+    	$closedTasksCount = $allTasksCount - $openTasksCount;
 
+    	$this->view->setVar('openTasksCount', $openTasksCount);
+    	$this->view->setVar('allTasksCount', $allTasksCount);
+   		$this->view->setVar('taskPercent', ceil (($closedTasksCount / $allTasksCount) * 100));
+
+   		$userTodaysTime = $this->currentUser->getTodaysTime();
+   		$userTodaysTimePercent = $this->currentUser->getTodaysTimePercent($userTodaysTime);
+   		$userMonthsTime = $this->currentUser->getMonthsTime();
+   		$userMonthsTimePercent = $this->currentUser->getMonthsTimePercent($userMonthsTime);
+   		$userTodaysProductivity = $this->currentUser->getTodaysProductivity($userTodaysTime);
+
+   		$this->view->setVar('userTodaysProductivity', $userTodaysProductivity);
+   		$this->view->setVar('userTodaysTime', $userTodaysTime);
+   		$this->view->setVar('userTodaysTimePercent', $userTodaysTimePercent);
+   		$this->view->setVar('userMonthsTime', $userMonthsTime);
+   		$this->view->setVar('userMonthsTimePercent', $userMonthsTimePercent);
+
+    	$this->view->setVar('body_id', 'dashboard');
+
+    	Phalcon\Tag::setTitle('Dashboard');
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -15,4 +15,146 @@ class User extends Phalcon\Mvc\Model
             return false;
         }
     }
+
+    public function getOpenTasksCount()
+    {
+        $openTasks = Task::find('assigned_to = "' . $this->id . '" AND status = "0"');
+
+        return count($openTasks);
+    }
+
+    public function getAllTasksCount()
+    {
+        $allTasks = Task::find('assigned_to = "' . $this->id . '"');
+
+        return count($allTasks);
+    }
+
+    public function getTodaysProductivity($todaysTime)
+    {
+        $start = 0;
+        $end = 0;
+
+        $attendances = Attendance::find('user_id = "' . $this->id . '" AND date = CURDATE()');
+
+        $i = 0;
+
+        foreach ($attendances AS $attendance) {
+            if ($i == 0) {
+                $start = strtotime($attendance->start);
+            }
+
+            if (is_null($attendance->end)) {
+                $end = time();
+            }
+            else {
+                $end = strtotime($attendance->end);
+            }
+
+            $i++;
+        }
+
+        $totalTodaysTimeStamp = $end - $start;
+
+        if ($totalTodaysTimeStamp == 0) {
+            return 0;
+        }
+
+        $oldTimeZone = date_default_timezone_get();
+        date_default_timezone_set('UTC');
+        $totalTodaysTime = date('H:i', $totalTodaysTimeStamp);
+        date_default_timezone_set($oldTimeZone);
+
+        $explode = explode(':', $todaysTime);
+        $_todaysTime = ($explode[0] * 60) + $explode[1];
+        $explode = explode(':', $totalTodaysTime);
+        $_totalTodaysTime = ($explode[0] * 60) + $explode[1];
+
+        return ceil(($_todaysTime / $_totalTodaysTime) * 100);
+    }
+
+    public function getTodaysTime()
+    {
+        $todaysTimeStamp = 0;
+
+        $attendances = Attendance::find('user_id = "' . $this->id . '" AND date = CURDATE()');
+
+        foreach ($attendances AS $attendance) {
+            $start = strtotime($attendance->start);
+
+            if (is_null($attendance->end)) {
+                $end = time();
+            }
+            else {
+                $end = strtotime($attendance->end);
+            }
+
+            $todaysTimeStamp += $end - $start;
+        }
+
+        $oldTimeZone = date_default_timezone_get();
+        date_default_timezone_set('UTC');
+        $todaysTime = date('H:i', $todaysTimeStamp);
+        date_default_timezone_set($oldTimeZone);
+
+        return $todaysTime;
+    }
+
+    public function getTodaysTimePercent($todaysTime)
+    {
+        $targetTime = 8;
+        $explode = explode(':', $todaysTime);
+
+        $_todaysTime = ($explode[0] * 60) + $explode[1];
+
+        $_targetTime = ($targetTime * 60);
+
+        return ceil(($_todaysTime / $_targetTime) * 100);
+    }
+
+    public function getMonthsTime()
+    {
+        $monthsTimeStamp = 0;
+
+        $attendances = Attendance::find('user_id = "' . $this->id . '" AND MONTH(date) = MONTH(NOW()) AND YEAR(date) = YEAR(NOW())');
+
+        foreach ($attendances AS $attendance) {
+            $start = strtotime($attendance->start);
+
+            if (is_null($attendance->end) && $attendance->date == date('Y-m-d')) {
+                $end = time();
+            }
+            else if (is_null($attendance->end)) {
+                $end = $start;
+            }
+            else {
+                $end = strtotime($attendance->end);
+            }
+
+            $monthsTimeStamp += $end - $start;
+        }
+
+        $oldTimeZone = date_default_timezone_get();
+        date_default_timezone_set('UTC');
+        $monthsTime = date('H:i', $monthsTimeStamp);
+        date_default_timezone_set($oldTimeZone);
+
+        return $monthsTime;
+    }
+
+    public function getMonthsTimePercent($monthsTime)
+    {
+        $daysTargetTime = 8;
+        $startDate = date('Y-m-d', mktime(0, 0, 0, date('m'), 1, date('Y')));
+        $endDate = date('Y-m-t', mktime(0, 0, 0, date('m'), 1, date('Y')));
+        $workingDays = AttendanceHelper::getWorkingDays($startDate, $endDate);
+
+        $explode = explode(':', $monthsTime);
+
+        $_monthsTime = ($explode[0] * 60) + $explode[1];
+
+        $_targetTime = ($daysTargetTime * $workingDays * 60);
+
+        return ceil(($_monthsTime / $_targetTime) * 100);
+    }
 }
