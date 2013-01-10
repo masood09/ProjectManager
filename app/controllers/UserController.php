d02cc9028a90571bab19df25972f007ce953d604
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-10 18:42:48 +0530

    Implementing login functionality.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,6 +4,27 @@ class UserController extends ControllerBase
 {
     public function loginAction()
     {
+        if ($this->request->isPost()) {
+            $email = $this->request->getPost('email', 'string');
+            $password = $this->request->getPost('password');
+
+            $user = User::findFirst('email="' . $email . '" AND is_active="1"');
+
+            if ($user) {
+                $Bcrypt = new Bcrypt();
+
+                if ($Bcrypt->verify($password, $user->password)) {
+                    $this->session->set('user_id', $user->id);
+                    $this->flashSession->success('Welcome ' . $user->full_name . '!');
+
+                    $this->response->redirect('dashboard/index/');
+                    $this->view->disable();
+                    return;
+                }
+            }
+
+            $this->flashSession->error('Email and/or password incorrect.');
+        }
 
         Phalcon\Tag::setTitle('Log in');
     }
