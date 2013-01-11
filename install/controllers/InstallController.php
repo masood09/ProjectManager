3b97d4fafabf970397236422aaa4839d87bbc18c
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-11 12:44:16 +0530

    We are checking if config file exists, if not we launch installation pages.
    Installation pages act as a separate application. We share library and
    helpers with main app.

---
+++ install/controllers/InstallController.php
@@ -0,0 +1,19 @@
+<?php
+
+class InstallController extends Phalcon\Mvc\Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function step1Action()
+    {
+
+    }
+
+    public function step2Action()
+    {
+
+    }
+}
