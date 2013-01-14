b4dec51d78cc9164fd8bcf6e5638c52d34a98e97
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-14 11:43:51 +0530

    Adding code to set the data_version and app_version in the config table.

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,8 +4,23 @@ class ControllerBase extends Phalcon\Mvc\Controller
 {
 	protected $currentUser = null;
 
+    protected function _checkSystem()
+    {
+        if (!Config::keyExists('core/data_version')) {
+            // If core/data_version does not exist, we set the value to 0.1.0
+            Config::setValue('core/data_version', '0.1.0');
+        }
+
+        if (!Config::keyExists('core/app_version')) {
+            // If core/data_version does not exist, we set the value to 0.1.0
+            Config::setValue('core/app_version', '0.1.0');
+        }
+    }
+
     protected function initialize()
     {
+        $this->_checkSystem();
+
     	$this->currentUser = User::findFirst('id = "' . $this->session->get('user_id') . '"');
     	$this->view->setVar('AppName', Config::getValue('core/name'));
     	$this->view->setVar('controller', $this->dispatcher->getControllerName());
