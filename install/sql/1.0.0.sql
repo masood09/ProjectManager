c7c8a3018d9cc0f321dfe3e350abc2f84b950707
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-14 13:10:33 +0530

    Adding sql file for version 1.0.0

---
+++ install/sql/1.0.0.sql
@@ -0,0 +1,21 @@
+DROP TABLE role;
+
+ALTER TABLE `task` DROP `description`;
+
+ALTER TABLE `attendance` DROP `total`;
+
+CREATE TABLE IF NOT EXISTS `notification` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`user_id` int(10) unsigned NOT NULL,
+	`title` varchar(255) NOT NULL,
+	`message` TEXT NOT NULL,
+	`project_id` int(10) unsigned NULL,
+	`task_id` int(10) unsigned NULL,
+	`comment_id` int(10) unsigned NULL,
+	`note_id` int(10) unsigned NULL,
+	`upload_id` int(10) unsigned NULL,
+	`read` tinyint(1) NOT NULL DEFAULT '0',
+	`created_by` int(10) unsigned NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
