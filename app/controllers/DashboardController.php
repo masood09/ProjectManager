933df4d80f76ad635b328b87f69d10a67ae1d0c3
Masood Ahmed <masood.ahmed09@gmail.com>
2013-01-15 08:13:31 +0530

    Converting indent to spaces.

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -4,27 +4,27 @@ class DashboardController extends ControllerBase
 {
     public function indexAction()
     {
-    	$openTasksCount = $this->currentUser->getOpenTasksCount();
-    	$allTasksCount = $this->currentUser->getAllTasksCount();
-    	$closedTasksCount = $allTasksCount - $openTasksCount;
+        $openTasksCount = $this->currentUser->getOpenTasksCount();
+        $allTasksCount = $this->currentUser->getAllTasksCount();
+        $closedTasksCount = $allTasksCount - $openTasksCount;
 
-    	$this->view->setVar('openTasksCount', $openTasksCount);
-    	$this->view->setVar('allTasksCount', $allTasksCount);
-   		$this->view->setVar('taskPercent', ceil (($closedTasksCount / $allTasksCount) * 100));
+        $this->view->setVar('openTasksCount', $openTasksCount);
+        $this->view->setVar('allTasksCount', $allTasksCount);
+        $this->view->setVar('taskPercent', ceil (($closedTasksCount / $allTasksCount) * 100));
 
-   		$userTodaysTime = $this->currentUser->getTodaysTime();
-   		$userTodaysTimePercent = $this->currentUser->getTodaysTimePercent($userTodaysTime);
-   		$userMonthsTime = $this->currentUser->getMonthsTime();
-   		$userMonthsTimePercent = $this->currentUser->getMonthsTimePercent($userMonthsTime);
-   		$userTodaysProductivity = $this->currentUser->getTodaysProductivity($userTodaysTime);
+        $userTodaysTime = $this->currentUser->getTodaysTime();
+        $userTodaysTimePercent = $this->currentUser->getTodaysTimePercent($userTodaysTime);
+        $userMonthsTime = $this->currentUser->getMonthsTime();
+        $userMonthsTimePercent = $this->currentUser->getMonthsTimePercent($userMonthsTime);
+        $userTodaysProductivity = $this->currentUser->getTodaysProductivity($userTodaysTime);
 
-   		$this->view->setVar('userTodaysProductivity', $userTodaysProductivity);
-   		$this->view->setVar('userTodaysTime', $userTodaysTime);
-   		$this->view->setVar('userTodaysTimePercent', $userTodaysTimePercent);
-   		$this->view->setVar('userMonthsTime', $userMonthsTime);
-   		$this->view->setVar('userMonthsTimePercent', $userMonthsTimePercent);
+        $this->view->setVar('userTodaysProductivity', $userTodaysProductivity);
+        $this->view->setVar('userTodaysTime', $userTodaysTime);
+        $this->view->setVar('userTodaysTimePercent', $userTodaysTimePercent);
+        $this->view->setVar('userMonthsTime', $userMonthsTime);
+        $this->view->setVar('userMonthsTimePercent', $userMonthsTimePercent);
 
-    	$this->view->setVar('body_id', 'dashboard');
+        $this->view->setVar('body_id', 'dashboard');
 
 
         $activities = Notification::find(array(
@@ -34,6 +34,6 @@ class DashboardController extends ControllerBase
 
         $this->view->setVar('activities', $activities);
 
-    	Phalcon\Tag::setTitle('Dashboard');
+        Phalcon\Tag::setTitle('Dashboard');
     }
 }
